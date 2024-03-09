2e85d1fbc0c86b06231b0087c53173b33240c7d5
Cui Tao <161183815+xHallo@users.noreply.github.com>
2024-03-09 22:15:10 +0800

    Prevented bot from playing song when not in channel

--- cogs/music.py
+++ cogs/music.py
@@ -9,8 +9,6 @@ import urllib.request
 import re
 import ffmpeg
 
-#if link is a direct link to video, return url. But if name of video is inputted, create link by searching for video on youtube,
-#taking closest result and creating url with querystring.
 def url_fetcher(key):
     if "://" in key:
         return key
@@ -24,7 +22,6 @@ def url_fetcher(key):
 
 yt_dlp.utils.bug_reports_message = lambda: ''
 
-#download audio file from youtube
 ytdl_format_options = {
     'format': 'bestaudio/best',
     'restrictfilenames': True,
@@ -38,13 +35,11 @@ ytdl_format_options = {
     'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
 }
 
-#novideo
 ffmpeg_options = {
     'options': '-vn'
 }
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 
-#download audio file from youtube
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
         super().__init__(source, volume)
@@ -73,34 +68,31 @@ class musicCommands(commands.Cog, name="Music"):
         await ctx.send("hello")
     @commands.command(name='join', help="Allows the commands to join the channel")
     async def join(self,ctx):
+        mevoicetrue = ctx.guild.me.voice
         if not ctx.message.author.voice:
             await ctx.send("{}, You are not connected to a channel".format(ctx.message.author.name))
             return
-        else:
-           channel = ctx.message.author.voice.channel
+        elif mevoicetrue is not None:
+            await ctx.send("I am already in a channel, switching channels now!")
+            channel = ctx.message.author.voice.channel
         await channel.connect()
 
     @commands.command(name="play", help ="Makes the commands play any song in youtube.")
     async def play(self,ctx,url):
-        """"   
-        voice_client = ctx.message.guild.voice_client
-        if  voice_client.is_connected() == False:
-            if not ctx.message.author.voice:
-                await ctx.send("{}, You are not connected to a channel.".format(ctx.message.author.name))
-                return
-            else:
-                channel = ctx.message.author.voice.channel
-            await channel.connect()
-        """
-        server=ctx.message.guild
-        voice_channel = server.voice_client
+        mevoicetrue = ctx.guild.me.voice
+        if not ctx.message.author.voice:
+            await ctx.send("You are not in a channel!")
+        elif mevoicetrue == None:
+            await ctx.send("I am not currently in a channel!")
+        else:
+            server=ctx.message.guild
+            voice_channel = server.voice_client
 
-        async with ctx.typing():
-            filename = await YTDLSource.from_url(url_fetcher(url),loop=self.bot.loop)
-            voice_channel.play(discord.FFmpegPCMAudio(executable=ffmpeg, source=filename)),
-        await ctx.send("Song request received, playing now.")
-        #except:
-            #await ctx.send("Bot is not connected")
+            async with ctx.typing():
+                filename = await YTDLSource.from_url(url_fetcher(url),loop=self.bot.loop)
+                voice_channel.play(discord.FFmpegPCMAudio(source=filename, executable = "D:\\Cui Tao Programming\\ffmpeg\\bin\\ffmpeg")),
+            await ctx.send("Song request received, playing now.")
+           
     @commands.command(name='pause', help='This command pauses the song')
     async def pause(self,ctx):
         voice_client = ctx.message.guild.voice_client
@@ -121,8 +113,11 @@ class musicCommands(commands.Cog, name="Music"):
 
     @commands.command(name='leave', help='To make the bot leave the voice channel')
     async def leave(self, ctx):
+        mevoicetrue = ctx.guild.me.voice
         voice_client = ctx.message.guild.voice_client
-        if voice_client.is_connected():
+        if mevoicetrue == None:
+            await ctx.send("You are not in a voice channel")
+        elif mevoicetrue is not None:
             await voice_client.disconnect()
         else:
             await ctx.send("Thank you for using me. Good bye!")
